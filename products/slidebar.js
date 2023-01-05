61c80f64fb7e1cf2c63b6c80e1e17bd14953c85d
Gaëlle Elias <eliasgaelle@gmailcom>
2023-01-05 13:55:44 +0100

    15_slidebar nog niet af

---
+++ products/slidebar.js
@@ -0,0 +1,27 @@
+let pos = 0;
+
+const leftBtn = document.querySelector("#slider .left");
+const rightBtn = document.querySelector("#slider .right");
+const slider = document.querySelector("#slider ul");
+//  var
+
+rightBtn.onclick = function () {
+  pos++;
+  animate();
+  leftBtn.classList.remove("hidden");
+  if (pos === 5) {
+    rightBtn.classList.add("hidden");
+  }
+};
+function animate() {
+  slider.style.marginLeft = -800 * pos + "px";
+}
+leftBtn.onclick = function () {
+  pos--;
+  animate();
+  rightBtn.classList.remove("hidden");
+
+  if (pos === 0) {
+    leftBtn.classList.add("hidden");
+  }
+};
